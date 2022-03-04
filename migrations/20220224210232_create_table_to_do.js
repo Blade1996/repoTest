d9fe1e84f67d260a082014687e1423c2bcdb275e
diegofalconi96 <diego.falconi4@utc.edu.ec>
2022-03-03 19:45:52 -0800

    fix(MIGRATIONS): Agregar Helpers en Migracion

--- migrations/20220224210232_create_table_to_do.js
+++ migrations/20220224210232_create_table_to_do.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const helper = require('./../src/shared/helperMigration');
+
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('to_do', (table) => {
 		table.increments().primary();
 		table.string('activity').nullable();
 		table.boolean('done').defaultTo(false);
+		helper.defaultColumns(table, false);
 	})
 };
 
